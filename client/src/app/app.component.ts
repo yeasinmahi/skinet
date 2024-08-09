b76c77946e16ffd0e1da3f7954cd626f033bde62
Yeasinmahi <Yeasinmahi72@yahoo.com>
2024-08-09 14:23:14 +0600

    Create a angular client

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -10,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'Skinet';
+  title = 'client';
 }
